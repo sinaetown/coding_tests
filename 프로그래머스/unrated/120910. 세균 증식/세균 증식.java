843683ec9e7b301a500989ac80cd79f56d1dfa62
sinaetown <123349044+sinaetown@users.noreply.github.com>
2023-11-20 20:49:01 +0900

    [level unrated] Title: 세균 증식, Time: 0.02 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 0;
+        for(int tt = 0; tt < t; tt++){
+            n*=2;
+            // System.out.print(n);
+        }
+        answer=n;
+        return answer;
+    }
+}
