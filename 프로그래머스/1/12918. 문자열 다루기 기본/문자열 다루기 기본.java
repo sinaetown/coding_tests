4b0925b9561aec238f8e28dc95a2884afa4e245d
sinaetown <123349044+sinaetown@users.noreply.github.com>
2023-12-07 17:16:54 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.17 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,19 +1,26 @@
-import java.lang.*;
+import java.util.*;
+import java.util.regex.Pattern;
 
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
-        int l = s.length();
-        if((l == 4) || (l == 6)){
-            for(int i = 0; i < l; i++){
-                if(!(Character.isDigit(s.charAt(i)))){
-                    return false;
-                }
-            }
-        }
-        else{
-            return false;
-        }
+         if(Pattern.matches("([0-9]{4})|([0-9]{6})", s)){
+          answer = true;
+      }
+      else{
+          answer = false;
+      }
+        // int l = s.length();
+        // if((l == 4) || (l == 6)){
+        //     for(int i = 0; i < l; i++){
+        //         if(!(Character.isDigit(s.charAt(i)))){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // else{
+        //     return false;
+        // }
         return answer;
     }
 }
