dd0317a364c30f1d3fbf9d68432cf4932e41be47
sinaetown <123349044+sinaetown@users.noreply.github.com>
2023-12-12 11:42:40 +0900

    [level unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        
+        int count = 0;
+        for(boolean b : finished){
+            if(!b){
+                count++;
+            }
+        }
+        
+        String[] answer = new String[count];
+        int index = 0;
+        for(int i = 0; i < finished.length; i++){
+            if(finished[i]==false){
+                answer[index++] = todo_list[i];
+            }
+        }
+        
+        return answer;
+    }
+}
