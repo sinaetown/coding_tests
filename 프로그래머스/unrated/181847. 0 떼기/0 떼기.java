9dbf506d61f9e2cc50ef11c18225cfda9659efff
sinaetown <123349044+sinaetown@users.noreply.github.com>
2023-12-15 22:18:06 +0900

    [level unrated] Title: 0 떼기, Time: 0.01 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,8 +1,6 @@
 class Solution {
     public String solution(String n_str) {
        String answer = "";
-//        "854020"
-//        "0010"
         int stop = 0;
         if (n_str.charAt(0) == '0') {
             for (int i = 1; i < n_str.length(); i++) {
@@ -20,10 +18,6 @@ class Solution {
             answer = n_str.substring(stop);
             return answer;
         }
-        // else if(stop==1){
-        //     answer= n_str;
-        //     return answer;
-        // }
         return answer;
 }
 }
--- 프로그래머스/unrated/181847. 0 떼기/README.md
+++ 프로그래머스/unrated/181847. 0 떼기/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 0 떼기 - 181847 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181847) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181847#) 
 
 ### 성능 요약
 
-메모리: 70.6 MB, 시간: 0.02 ms
+메모리: 72.4 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 10:27:32
+2023년 12월 5일 22:18:1
 
 ### 문제 설명
 
