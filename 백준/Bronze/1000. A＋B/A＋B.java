20cc0a8d3a66fcfb01d5003307e4a309bc1b38dc
sinaetown <123349044+sinaetown@users.noreply.github.com>
2023-12-18 17:45:37 +0900

    [Bronze V] Title: A+B, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        System.out.println(a + b);
+
+    }
+}
+
