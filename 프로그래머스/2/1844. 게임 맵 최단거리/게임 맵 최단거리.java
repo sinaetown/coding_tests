bbd71406c968c133534ae96a740cdf9e8e04d6e3
sinaetown <123349044+sinaetown@users.noreply.github.com>
2023-12-29 14:14:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.75 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -0,0 +1,50 @@
+import java.util.*;
+class Solution {
+     static boolean[][] visited;
+    static List<List<Integer>> adjList = new ArrayList<>();
+    static int n;
+    static int m;
+    static int[][] static_maps;
+    public int solution(int[][] maps) {
+        static_maps = maps;
+        int answer = 0;
+         n = static_maps.length;
+        m = static_maps[0].length;
+
+        visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+        }
+
+        bfs(0, 0);
+        if(visited[n-1][m-1]){
+            answer = static_maps[n - 1][m - 1];
+        }
+        else{
+            answer = -1;
+        }
+        return answer;
+    }
+
+    static void bfs(int a, int b) {
+        int[] dx = {-1, 1, 0, 0}; //좌우상하
+        int[] dy = {0, 0, -1, 1};
+        Queue<List<Integer>> queue = new LinkedList<>();
+        visited[a][b] = true;
+        queue.add(new ArrayList<>(Arrays.asList(a, b)));
+        while (!queue.isEmpty()) {
+            List<Integer> now = queue.poll();
+            for (int d = 0; d < 4; d++) {
+                int x = now.get(0) + dx[d];
+                int y = now.get(1) + dy[d];
+                if (x >= 0 && y >= 0 && x < n && y < m) {
+                    if (!visited[x][y] && static_maps[x][y] != 0) {
+                        visited[x][y] = true;
+                        static_maps[x][y] = static_maps[now.get(0)][now.get(1)] + 1;
+                        queue.add(new ArrayList<>(Arrays.asList(x, y)));
+                    }
+                }
+            }
+        }
+    }
+}
