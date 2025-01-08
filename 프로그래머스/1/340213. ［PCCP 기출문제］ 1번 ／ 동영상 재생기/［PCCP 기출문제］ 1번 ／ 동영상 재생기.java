2b5ab7e04fab94d36c45c7bbac29475e7b3ce7ba
sinaetown <123349044+sinaetown@users.noreply.github.com>
2025-01-08 13:31:19 -0500

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 12.44 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        String answer = "";
+        int videoSeconds = toSeconds(video_len);
+        int posSeconds = toSeconds(pos);
+        int startSeconds = toSeconds(op_start);
+        int endSeconds = toSeconds(op_end);
+        if ((startSeconds <= posSeconds) && (posSeconds <= endSeconds)) posSeconds = endSeconds;
+        for (int c = 0; c < commands.length; c++) {
+            if (commands[c].equals("next")) {
+                posSeconds += 10;
+            } else if (commands[c].equals("prev")) {
+                posSeconds -= 10;
+            }
+            System.out.println(posSeconds);
+            if (posSeconds < 10) posSeconds = 0;
+            if (videoSeconds - posSeconds < 10) posSeconds = videoSeconds;
+            if ((startSeconds <= posSeconds) && (posSeconds <= endSeconds)) posSeconds = endSeconds;
+        }
+        String min = (Integer.toString(posSeconds / 60).length() == 1) ? "0" + Integer.toString(posSeconds / 60) : Integer.toString(posSeconds / 60);
+        String sec = (Integer.toString(posSeconds % 60).length() == 1) ? "0" + Integer.toString(posSeconds % 60) : Integer.toString(posSeconds % 60);
+        answer = min + ":" + sec;
+        return answer;
+    }
+
+    public int toSeconds(String s) {
+        String[] arr = s.split(":");
+        return Integer.parseInt(arr[0]) * 60 + Integer.parseInt(arr[1]);
+    }
+}
