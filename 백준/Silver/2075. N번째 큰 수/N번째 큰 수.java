fdc2a3ff99a1d245ae227d03ac355390cdffa033
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-03-13 20:42:50 -0400

    [Silver III] Title: N번째 큰 수, Time: 804 ms, Memory: 274860 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                pq.add(Integer.parseInt(st.nextToken()));
+            }
+        }
+        for (int i = 0; i < n - 1; i++) pq.poll();
+        System.out.println(pq.poll());
+    }
+}
