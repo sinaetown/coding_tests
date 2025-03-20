343f3626997b51ac751ba6836095755c7a52ca88
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-03-20 02:50:02 -0400

    [Gold V] Title: 회의실 배정, Time: 516 ms, Memory: 45544 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        List<int[]> list = new ArrayList<>();
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list.add(new int[]{start, end});
+        }
+        Collections.sort(list, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            }
+        });
+        int y = -1;
+        int cnt = 0;
+        for (int[] i : list) {
+            if (y <= i[0]) {
+                cnt++;
+                y = i[1];
+            }
+        }
+        System.out.println(cnt);
+    }
+}
