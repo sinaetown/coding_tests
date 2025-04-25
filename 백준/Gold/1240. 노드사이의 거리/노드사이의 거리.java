d5914fa594972aa3b74eeda50ee7d67a171b9b0b
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-04-25 15:29:01 -0400

    [Gold V] Title: 노드사이의 거리, Time: 272 ms, Memory: 33544 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -1,50 +1,67 @@
+
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static class Node {
+        int num;
+        int weight;
+
+        public Node(int num, int weight) {
+            this.num = num;
+            this.weight = weight;
+        }
+    }
+
+    static List<List<Node>> list = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        final int INF = 1000000000;
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] grid = new int[n + 1][n + 1];
-//        boolean[][] visited = new boolean[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (i == j) grid[i][j] = 0;
-                else grid[i][j] = INF;
-            }
+        for (int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
         }
         for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
             int one = Integer.parseInt(st.nextToken());
             int two = Integer.parseInt(st.nextToken());
             int weight = Integer.parseInt(st.nextToken());
-            grid[one][two] = weight;
-            grid[two][one] = weight;
-//            visited[one][two] = visited[two][one] = true;
+            list.get(one).add(new Node(two, weight));
+            list.get(two).add(new Node(one, weight));
         }
-        floyd(grid, n);
+
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int one = Integer.parseInt(st.nextToken());
             int two = Integer.parseInt(st.nextToken());
-            bw.write(grid[one][two] + "\n");
+            bw.write(bfs(one, two, new boolean[n + 1], n) + "\n");
         }
-//        System.out.println(Arrays.deepToString(grid));
         bw.flush();
         bw.close();
     }
 
-    public static void floyd(int[][] grid, int n) {
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
-                    grid[i][j] = Math.min(grid[i][k] + grid[k][j], grid[i][j]);
+    public static int bfs(int start, int end, boolean[] visited, int n) {
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.add(new Node(start, 0));
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
+            int nowNum = now.num;
+            int nowWeight = now.weight;
+            if (nowNum == end) return nowWeight;
+            for (int i = 0; i < list.get(nowNum).size(); i++) {
+                Node next = list.get(nowNum).get(i);
+                int nextNum = next.num;
+                int nextWeight = next.weight;
+                if (!visited[nextNum]) {
+                    visited[nextNum] = true;
+                    queue.add(new Node(nextNum, nowWeight + nextWeight));
                 }
             }
         }
+        return -1;
     }
 }
