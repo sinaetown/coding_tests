39264cd786dc17d7f969bbc1503be1ce2d80fa33
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-06-30 19:57:39 +0900

    [level 2] Title: 점 찍기, Time: 15.38 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(int k, int d) {
+        long answer = 0;
+        for(long x = 0; x <= d; x+=k){
+            long y = (long) Math.sqrt((long)d*d - (long)x*x);
+            answer += y / k + 1;
+        }
+        return answer;
+    }
+}
