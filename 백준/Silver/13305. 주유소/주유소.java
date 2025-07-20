056f43e800911711b71d263dd6a6cddb0712fb27
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-07-20 22:12:15 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 30604 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] road;
+    public static int[] fuel;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        road = new int[n - 1];
+        fuel = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n - 1; i++) {
+            road[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            fuel[i] = Integer.parseInt(st.nextToken());
+        }
+        dec();
+        int answer = 0;
+        for (int i = 0; i < road.length; i++) answer += road[i] * fuel[i];
+        System.out.println(answer);
+    }
+
+    public static void dec() {
+        for (int i = 0; i < fuel.length - 1; i++) {
+            if (fuel[i] < fuel[i + 1]) {
+                fuel[i + 1] = fuel[i];
+            }
+        }
+    }
+}
