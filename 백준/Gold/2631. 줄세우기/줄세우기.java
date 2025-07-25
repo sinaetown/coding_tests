8edcd914b64a1a1606ce9255a00cc838e11b070a
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-07-25 09:25:22 +0900

    [Gold IV] Title: 줄세우기, Time: 108 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+        System.out.println(n - max);
+    }
+}
