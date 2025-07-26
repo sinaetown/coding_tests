c32d3489cc5e875c8ba4266d9ff583ef0430f11a
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-07-26 11:39:44 +0900

    [Gold IV] Title: 줄세우기, Time: 104 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -8,16 +8,15 @@ public class Main {
         int[] arr = new int[n];
         int[] dp = new int[n];
         for (int i = 0; i < n; i++) arr[i] = Integer.parseInt(br.readLine());
-
         int max = 0;
         for (int i = 0; i < n; i++) {
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
-                if (arr[j] < arr[i] && dp[j] + 1 > dp[i]) {
-                    dp[i] = dp[j] + 1;
-                    max = Math.max(max, dp[i]);
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
+            max = Math.max(max, dp[i]);
         }
         System.out.println(n - max);
     }
