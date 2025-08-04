ca18f910c9e56f134474b74787c1faa49d1addc4
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-08-04 17:29:53 +0900

    [Silver III] Title: IF문 좀 대신 써줘, Time: 472 ms, Memory: 52108 KB -BaekjoonHub

--- 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
+++ 백준/Silver/19637. IF문 좀 대신 써줘/IF문 좀 대신 써줘.java
@@ -2,17 +2,19 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static BufferedWriter bw;
+    public static int N;
+    public static int M;
+    public static int[] degree;
+    public static String[] lv;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        String[] lv = new String[N];
-        int[] degree = new int[N];
-        degree[0] = 0;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        lv = new String[N];
+        degree = new int[N];
         for (int n = 0; n < N; n++) {
             st = new StringTokenizer(br.readLine());
             lv[n] = st.nextToken();
@@ -20,23 +22,22 @@ public class Main {
         }
         for (int m = 0; m < M; m++) {
             int given = Integer.parseInt(br.readLine());
-            bw.write(lv[print(lv, degree, given)] + "\n");
+            bw.write(bs(given) + "\n");
         }
         bw.flush();
-        bw.close();
     }
 
-    public static int print(String[] lv, int[] degree, int given) throws IOException {
-        int low = 0;
-        int high = degree.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (given > degree[mid]) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
+    public static String bs(int target) {
+        int left = 0;
+        int right = N - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (degree[mid] < target) {
+                left = mid + 1;
+            } else right = mid - 1;
         }
-        return low;
+        return lv[left];
     }
+
+
 }
