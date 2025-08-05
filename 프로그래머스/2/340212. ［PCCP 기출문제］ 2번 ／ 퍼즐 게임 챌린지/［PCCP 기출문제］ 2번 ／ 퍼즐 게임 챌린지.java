c69b7fc2e492f6345e191230ee1c5366a4f2b99f
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-08-05 15:35:16 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 49.12 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,32 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
-        return bs(diffs, times, limit);
-    }
-    
-    public int bs(int[] diffs, int[] times, long limit){
-        int max =  1;
-        int min = 100000;
-        while(max <= min){
-            int level = (max+min)/2;
-            long mid = cal(diffs, times, level);
-            if(mid > limit){
-                max = level+1;
+        long left = 1;
+        long right = 0;
+        for(int i : diffs) right = Math.max(right, i);
+        while(left <= right){
+            long mid = (left+right)/2;
+            // 시간이 너무 많이 걸리면 숙련도를 높여
+            if(solve(mid, diffs, times) > limit){ 
+                left = mid + 1;
             }
-            else min = level-1;
-        }
-        return max;
-    }
-    
-    public long cal(int[] diffs, int[] times, int level){
-        long sum = 0;
-        for(int i = 0; i < diffs.length; i++){
-            if(diffs[i] <= level){
-                sum += times[i];
-            }
-            else{
-                sum += (long)(times[i-1]+times[i])*(long)(diffs[i]-level) + times[i];
+            else {
+                right = mid - 1;
             }
         }
-        return sum;
+        return (int) left;
+    }
+    
+    public long solve(long lv, int[] diffs, int[] times){
+        long ans = 0; // 걸리는 시간
+        // 시간이 많이 걸리는 이유는 숙련도가 낮기 때문
+        ans += times[0];
+        for(int i = 1; i < diffs.length; i++){ 
+            if(diffs[i] <= lv) ans+=times[i];
+            else{ //처음이 틀리는 경우는 없음
+                long wrong = diffs[i]-lv; 
+                long prev = times[i-1];
+                long current = times[i];
+                ans += ((prev + current) * wrong + current);
+            }
+        }
+        return ans;
     }
 }
