ee45b7298d6a09032e6eae1f3d278b6d6ad093f0
Sinae Hong 홍신애 <123349044+sinaetown@users.noreply.github.com>
2025-08-08 16:38:56 +0900

    [level 2] Title: 모음 사전, Time: 12.75 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
 class Solution {
-   String word;
     String[] alpha = {"A", "E", "I", "O", "U"};
     List<String> list = new ArrayList<>();
-
+    
     public int solution(String word) {
-        this.word = word;
-        perm("", 0);
+        int answer = 0;
+        permutation(0,"",word);
         return list.indexOf(word);
     }
-
-    public  void perm(String str, int depth) {
-        list.add(str);
-        if (depth == 5) return;
-        for (int i = 0; i < alpha.length; i++) perm(str + alpha[i], depth + 1);
+    
+    public void permutation(int depth, String res, String word){
+        list.add(res);
+        if(depth == 5) return;
+        for(int i = 0; i < 5; i++) permutation(depth+1, res+alpha[i] ,word);
     }
 }
